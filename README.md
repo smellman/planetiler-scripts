dcb5e0585912092bc6c49fae153a8af79cc7d6d0
Taro Matsuzawa <btm@tech.email.ne.jp>
2022-07-13 15:50:22 +0900

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Generate Planet Vector Tile
+
+## usage
+
+```bash
+wget https://github.com/onthegomap/planetiler/releases/download/v0.5.0/planetiler.jar
+./runworld.sh
+```
---
+++ runworld.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+java -Xmx20g \
+  -jar planetiler.jar \
+  `# Download the latest planet.osm.pbf from s3://osm-pds bucket` \
+  --area=planet --bounds=world --download \
+  `# Accelerate the download by fetching the 10 1GB chunks at a time in parallel` \
+  --download-threads=10 --download-chunk-size-mb=1000 \
+  `# Also download name translations from wikidata` \
+  --fetch-wikidata \
+  --mbtiles=output.mbtiles \
+  --nodemap-type=array --nodemap-storage=mmap 2>&1 | tee logs.txt
