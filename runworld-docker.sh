1203521a7c03d388a2ca9fcf535e345d13c5ef90
Taro Matsuzawa <btm@tech.email.ne.jp>
2024-08-14 13:59:38 +0900

    use planetiler 0.8.2, set user and group

--- runworld-docker.sh
+++ runworld-docker.sh
@@ -2,7 +2,7 @@
 set -e
 #java -Xmx24g \
 #  -jar planetiler.jar \
-docker run -e JAVA_TOOL_OPTIONS="-Xmx24g" -v "$(pwd)/data":/data ghcr.io/onthegomap/planetiler:0.8.1 \
+docker run -e JAVA_TOOL_OPTIONS="-Xmx24g" -u `id -u`:`id -g` -v "$(pwd)/data":/data ghcr.io/onthegomap/planetiler:0.8.2 \
   -Xmx24g \
   `# Download the latest planet.osm.pbf from s3://osm-pds bucket` \
   --area=planet --bounds=world --download \
