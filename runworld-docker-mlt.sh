f854fbd3bfa79327a7388b5f17038dd6f692a345
Taro Matsuzawa aka. btm <btm@smellman.org>
2025-11-02 08:54:46 +0900

    test: maplibre tile spec

---
+++ runworld-docker-mlt.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+#java -Xmx24g \
+#  -jar planetiler.jar \
+docker run -e JAVA_TOOL_OPTIONS="-Xmx32g" -u `id -u`:`id -g` -v "$(pwd)/data":/data ghcr.io/onthegomap/planetiler:0.9.3 \
+  -Xmx32g \
+  `# Download the latest planet.osm.pbf from s3://osm-pds bucket` \
+  --area=planet --bounds=world --download \
+  `# Accelerate the download by fetching the 10 1GB chunks at a time in parallel` \
+  --download-threads=10 --download-chunk-size-mb=1000 \
+  --mbtiles=data/output-mlt.pmtiles \
+  --tile-format=mlt \
+  --nodemap-type=array --nodemap-storage=mmap 2>&1 | tee logs.txt
